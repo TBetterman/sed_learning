16d14573b323878cbb98fd9dde02f3cdefc3a4c2
Everglow26 <214672670@qq.com>
2019-06-18 14:55:07 +0800

    init project

---
+++ build/CMakeFiles/sed_learning.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sed_learning.dir/main.cpp.o"
+  "sed_learning.pdb"
+  "sed_learning"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sed_learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
